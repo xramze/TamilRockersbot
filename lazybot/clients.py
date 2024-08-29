3d2a3ec4c18f2112097fe647d3c7da8a32968bcc
Ram <166788107+xramze@users.noreply.github.com>
2024-08-29 14:49:01 +0530

    Update clients.py

--- lazybot/clients.py
+++ lazybot/clients.py
@@ -1,9 +1,9 @@
-    # Credit @LazyDeveloper.
+    # Credit @TamilRockers_LinkZz.
     # Please Don't remove credit.
-    # Born to make history @LazyDeveloper !
+    # Born to make history @TamilRockers_LinkZz !
 
     # Thank you LazyDeveloper for helping us in this Journey
-    # 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
+    # 🥰  Thank you for giving me credit @TamilRockers_LinkZz  🥰
 
     # for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
 import asyncio
