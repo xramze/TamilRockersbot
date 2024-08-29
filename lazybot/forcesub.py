c94f87a278a51ad6d2acbae1c20dca64a44efd50
Ram <166788107+xramze@users.noreply.github.com>
2024-08-29 14:49:49 +0530

    Update forcesub.py

--- lazybot/forcesub.py
+++ lazybot/forcesub.py
@@ -1,5 +1,5 @@
-# with Love @LazyDeveloperr 💘
-# Subscribe YT @LazyDeveloperr - to learn more about this for free...
+# with Love @TamilRockers_LinkZz 💘
+# Subscribe @TamilRockers_LinkZz - to learn more about this for free...
 
 import asyncio
 from pyrogram import Client
